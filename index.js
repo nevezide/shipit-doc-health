8919ec6219f3b0ef33102d77d6764c6537af74f4
Fabrice <nevezide44@gmail.com>
2016-10-12 16:48:59 +0200

    my first bot action

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let Wit = null;
+let interactive = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  interactive = require('../').interactive;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  interactive = require('node-wit').interactive;
+}
+
+const accessToken = (() => {
+  if (process.argv.length !== 3) {
+    console.log('usage: node index.js <wit-access-token>');
+    process.exit(1);
+  }
+  return process.argv[2];
+})();
+
+const actions = {
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    return new Promise(function(resolve, reject) {
+      console.log('sending...', JSON.stringify(response));
+      return resolve();
+    });
+  },
+  getContactsPerHour({context, entities}) {
+    return new Promise(function(resolve, reject) {
+      console.log(context);
+
+      return resolve({
+        contacts: 45,
+        "client.name": 'Maty',
+        "client.id":2
+      });
+    });
+  },
+};
+
+const client = new Wit({accessToken, actions});
+interactive(client);
