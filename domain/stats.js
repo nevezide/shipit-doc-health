32d658f8661a9affde5b58c431cc0cb27fdf6611
Fabrice <nevezide44@gmail.com>
2016-10-13 20:42:46 +0200

    improve data format

--- domain/stats.js
+++ domain/stats.js
@@ -2,7 +2,7 @@
 
 module.exports = (publicApi) => {
   return {
-    getContactsPerHour: (clientName, channel, resource, resource_id, indicators, from, to, granularity) => {
+    getContactsPerHour: (clientName, indicators, from, to, channel, resource, resource_id, granularity) => {
       const platform = 'ha', website_list='3';
 
       // TODO get website_list from client name (on each platform)
--- wit/actions/stats.js
+++ wit/actions/stats.js
@@ -14,101 +14,66 @@ module.exports = (statsDomain) => {
     return typeof val === 'object' ? val.value : val;
   };
 
-  const parseEntities = (schema, entities) => {
-    const outputParams = {};
-    const missingParams = {};
-
-    _.map(_.keys(schema), (entityName) => {
-      const entityValue = getEntity(entities, entityName);
-      if (_.isEmpty(entityValue) && schema[entityName].required) {
-        missingParams['missing' + _.upperFirst(entityName)] = true;
-        return;
-      }
-      outputParams[entityName] = entityValue;
-    });
-
-    if(_.isEmpty(missingParams)) {
-      return when.resolve(outputParams);
-    }
-
-    return when.reject(missingParams);
-  };
-
   return {
-    getContactsPerHour({context, entities}) {
-      const schema = {
-        clientName: {
-          required: true
-        },
-        channel: {
-          required: false
-        },
-        resource: {
-          required: false
-        },
-        resource_id: {
-          required: false
-        },
-        indicators: {
-          required: true
-        },
-        datetime: {
-          required: false
-        },
-        from: {
-          required: false
-        },
-        to: {
-          required: false
-        },
-        granularity: {
-          required: false
-        }
-      };
+    getContactsPerHour: ({context, entities}) => {
+      return when(entities)
+        .then((entities) => {
+          const params = {
+            clientName: getEntity(entities, 'clientName'),
+            indicators: getEntity(entities, 'indicators'),
+            datetime: getEntity(entities, 'datetime'),
+            from: getEntity(entities, 'from'),
+            to: getEntity(entities, 'to'),
+            channel: getEntity(entities, 'channel'),
+            resource: getEntity(entities, 'resource'),
+            resource_id: getEntity(entities, 'resource_id'),
+            granularity: getEntity(entities, 'granularity')
+          };
 
-      /*
-       channel='chat', resource='operator';
-       const resource_id='3133', indicators='contact_per_hour_number', from='2016-10-01';
-       const to='2016-10-11', granularity='day';
-       */
+          if (_.isEmpty(params.clientName)) {
+            context.missingClientName = true;
+            return when.resolve(context);
+          }
+          if (_.isEmpty(params.indicators)) {
+            context.missingIndicators = true;
+            return when.resolve(context);
+          }
+          if (!(_.isEmpty(params.datetime) || (_.isEmpty(params.from) && _.isEmpty(params.to)))) {
+            context.missingPeriod = true;
+            return when.resolve(context);
+          }
+          if (!_.isEmpty(params.datetime)) {
+            params.from = params.datetime;
+            params.to = params.datetime;
+            delete params.datetime;
+          }
 
-      //Parse entities
-      return parseEntities(schema, entities)
-      .then((params) => {
-        if (!(_.isEmpty(params.datetime) || (_.isEmpty(params.from) && _.isEmpty(params.to)))) {
-          return when.reject({
-            missingPeriod: true
+          params.from = _.truncate(params.from, {
+            length: 10,
+            omission: ''
           });
-        }
-        if (!_.isEmpty(params.datetime)) {
-          params.from = params.datetime;
-          params.to = params.datetime;
-          delete params.datetime;
-        }
 
-        params.from = _.truncate(params.from, {
-          length: 10,
-          omission: ''
+          params.to = _.truncate(params.to, {
+            length: 10,
+            omission: ''
+          });
+
+          params.indicators = _.snakeCase(params.indicators);
+
+          return when(_.values(params))
+            .spread(statsDomain.getContactsPerHour)
+            .then((data) => {
+              const output = {
+                data: _.round(_.values(data)[0], 2)
+              };
+              params.indicators = _.lowerCase(params.indicators);
+
+              return _.merge(context, params, output);
+            });
+        })
+        .catch((error) => {
+          return {error};
         });
-
-        params.to = _.truncate(params.to, {
-          length: 10,
-          omission: ''
-        });
-
-        params.indicators = _.snakeCase(params.indicators);
-
-        return _.values(params);
-      })
-      .catch((missing) => {
-        return missing;
-      })
-      .spread(statsDomain.getContactsPerHour)
-      .then((data) => {
-        console.log(data);
-        context.data = _.values(data)[0];
-        return context;
-      });
     }
   };
 };
